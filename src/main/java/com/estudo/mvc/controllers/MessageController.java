268fc3d2e035e356bb06ac88a0f69681ca3b31b4
Filipe Camera <filipecameracastro@gmail.com>
2022-08-28 21:22:20 -0300

    fazendo alguns ajustes

--- src/main/java/com/estudo/mvc/controllers/MessageController.java
+++ src/main/java/com/estudo/mvc/controllers/MessageController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 import javax.validation.Valid;
 
--- src/main/java/com/estudo/mvc/repository/RoleRepository.java
+++ src/main/java/com/estudo/mvc/repository/RoleRepository.java
@@ -1,6 +1,5 @@
 package com.estudo.mvc.repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
